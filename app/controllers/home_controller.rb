1960f8e5a356acd951549d39d01e4ea461508450
Phanikanth <phanikanth@intellolabs.com>
2023-09-03 18:33:29 +0530

    Giving -all- option in the hotels selection dropdown

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -19,23 +19,32 @@ class HomeController < ApplicationController
         start_date = Date.parse(checkin)
         end_date = Date.parse(checkout)
         hotel_id = params["hotel_id"]
+        location = params["location"]
 
-        bookings_response = []
         original_start_date = start_date
-        loop do
-            diff = (end_date - start_date).to_i
-            diff_considered = diff > 6 ? 6 : diff
-            checkin = start_date.strftime("%F")
-            checkout = start_date + diff_considered.days
-            resp = BookingsHelper.fetch_response(hotel_id, checkin, checkout)
-            bookings_response += resp unless resp.nil?
-            start_date += (diff_considered + 1).days
-            break if start_date > end_date
+        if hotel_id == 'all'
+            hotels = Hotel.where(location: location)
+        else
+            hotels = Hotel.where(id: hotel_id)
         end
-        @hotel_status = bookings_response.nil? ? {} : BookingsHelper.parse_response(original_start_date, end_date, bookings_response)
+        @all_hotel_status = {}
+        hotels.each do |hotel|
+            bookings_response = []
+            loop do
+                diff = (end_date - start_date).to_i
+                diff_considered = diff > 6 ? 6 : diff
+                checkin = start_date.strftime("%F")
+                checkout = start_date + diff_considered.days
+                resp = BookingsHelper.fetch_response(hotel.id, checkin, checkout)
+                bookings_response += resp unless resp.nil?
+                start_date += (diff_considered + 1).days
+                break if start_date > end_date
+            end
+            @all_hotel_status[hotel.name] = bookings_response.nil? ? {} : BookingsHelper.parse_response(original_start_date, end_date, bookings_response)
+        end
         @dates = original_start_date..end_date
         @room_types = RoomType.all
-        puts "hotel_status : #{@hotel_status}"
+        puts "hotel_status : #{@all_hotel_status}"
 
         respond_to do |format|
             format.js
