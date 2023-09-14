d6b373b916db6c490cf8db30ee6d1c770174145d
Phanikanth <phanikanth@intellolabs.com>
2023-09-14 22:57:11 +0530

    Restricting the users to open the page without client id

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, skip: 'registrations', controllers: {
     sessions: 'users/sessions'
   }
+  get 'cid/:client_id', to: 'home#index'
   get '/submit', to: 'home#submit'
   root to: "home#index"
 end
